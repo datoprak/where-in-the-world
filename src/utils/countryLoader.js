4a146c41cc999ffc67df675600f74df310240051
datoprak <106121887+datoprak@users.noreply.github.com>
2023-10-15 01:16:46 +0300

    Fix partial search issue

--- src/utils/countryLoader.js
+++ src/utils/countryLoader.js
@@ -1,6 +1,8 @@
 import { fetchData } from "./fetchData";
 
 export default async function loader({ params }) {
-  const data = await fetchData(`https://restcountries.com/v3.1/name/${params.name}`);
+  const data = await fetchData(
+    `https://restcountries.com/v3.1/name/${params.name}?fullText=true`,
+  );
   return data;
 }
