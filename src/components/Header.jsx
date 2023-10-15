252b43bf2da4e8a1aba55533bf53f8e9e9f9378d
datoprak <106121887+datoprak@users.noreply.github.com>
2023-10-16 01:39:46 +0300

    Create dark&light theme switch with tailwind and localstorage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,42 @@
+import { useEffect, useState } from "react";
+
 const Header = () => {
+  const [theme, setTheme] = useState(null);
+
+  useEffect(() => {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      document.documentElement.classList.add("dark");
+      setTheme("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      setTheme("light");
+    }
+  }, []);
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else if (theme === "light") {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    } else {
+      localStorage.removeItem("theme");
+    }
+  }, [theme]);
+
+  const handleTheme = () => {
+    setTheme(prev => (prev === "dark" ? "light" : "dark"));
+  };
+
   return (
     <nav>
-      <div className="logo">
+      <div className="logo dark:bg-black dark:text-white">
         <h1>Where in the World?</h1>
       </div>
-      <button className="theme-button">Theme</button>
+      <button className="theme-button" onClick={handleTheme}>
+        Go to {theme === "dark" ? "light" : "dark"} theme
+      </button>
     </nav>
   );
 };
