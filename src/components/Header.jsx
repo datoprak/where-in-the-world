fa49b7728dd0771695a833f6f9f8842b49c4231c
datoprak <106121887+datoprak@users.noreply.github.com>
2023-10-16 02:40:40 +0300

    Create header styling and add fontawesome

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
 import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEarthEurope,
+  faMoon,
+  faSun,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
-  const [theme, setTheme] = useState(null);
+  const [theme, setTheme] = useState(localStorage.getItem("theme"));
 
   useEffect(() => {
-    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
       document.documentElement.classList.add("dark");
       setTheme("dark");
     } else {
@@ -30,12 +40,17 @@ const Header = () => {
   };
 
   return (
-    <nav>
-      <div className="logo">
-        <h1>Where in the World?</h1>
+    <nav className="flex items-center justify-between p-8">
+      <div className="flex items-center gap-3">
+        <FontAwesomeIcon icon={faEarthEurope} size="2xl" />
+        <h1 className="text-4xl font-extrabold">Where in the World?</h1>
       </div>
-      <button className="theme-button" onClick={handleTheme}>
-        Go to {theme === "dark" ? "light" : "dark"} theme
+      <button onClick={handleTheme}>
+        {theme === "dark" ? (
+          <FontAwesomeIcon icon={faSun} size="2xl" />
+        ) : (
+          <FontAwesomeIcon icon={faMoon} size="2xl" />
+        )}
       </button>
     </nav>
   );
