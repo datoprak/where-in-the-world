755c3e6bcf0189e02db03020f7543ee862e1a344
datoprak <106121887+datoprak@users.noreply.github.com>
2023-10-17 10:36:10 +0300

    Change theme button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -40,19 +40,23 @@ const Header = () => {
         <Icon name="earth" className="h-10 w-10" />
         <h1 className="text-4xl font-extrabold">Where in the World?</h1>
       </div>
-      <button onClick={handleTheme} className="flex items-center gap-2">
-        {theme === "dark" ? (
-          <>
-            <Icon name="sun" className="h-8 w-8" />
-            <span className="text-xl font-semibold">Light Mode</span>
-          </>
-        ) : (
-          <>
-            <Icon name="moon" className="h-8 w-8" />
-            <span className="text-xl font-semibold">Dark Mode</span>
-          </>
-        )}
-      </button>
+      <div className="flex items-center gap-2">
+        <label className="swap swap-rotate">
+          <input
+            type="checkbox"
+            checked={theme === "dark"}
+            onChange={handleTheme}
+          />
+          <Icon name="sun" className="swap-on h-10 w-10 fill-current" />
+          <Icon name="moon" className="swap-off h-10 w-10 fill-current" />
+        </label>
+        <span
+          onClick={handleTheme}
+          className="cursor-pointer select-none text-xl font-semibold"
+        >
+          {theme === "dark" ? "Light Mode" : "Dark Mode"}
+        </span>
+      </div>
     </nav>
   );
 };
